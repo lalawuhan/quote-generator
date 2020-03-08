3852160d7836fa83e252efc2d96da755aaa57609
lalawuhan <precious.segoe@gmail.com>
2020-03-08 01:08:54 +0100

    Updates

---
+++ precache-manifest.abd145ee7e9d165fe769f47bdb8cf853.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921cd8225043a3e8242b3c86beb754a5",
+    "url": "/quote-generator/index.html"
+  },
+  {
+    "revision": "62ebe65513a31fb0829d",
+    "url": "/quote-generator/static/css/main.56dc0dae.chunk.css"
+  },
+  {
+    "revision": "e882c8833b7bc70ce6cb",
+    "url": "/quote-generator/static/js/2.1043edd1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/quote-generator/static/js/2.1043edd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62ebe65513a31fb0829d",
+    "url": "/quote-generator/static/js/main.8951a0d1.chunk.js"
+  },
+  {
+    "revision": "359fb28907128cb95ab7",
+    "url": "/quote-generator/static/js/runtime-main.96fd52a4.js"
+  },
+  {
+    "revision": "bf4f92c71e8131cabd73b3a7b09e0b6d",
+    "url": "/quote-generator/static/media/women.bf4f92c7.svg"
+  }
+]);
